96e5ef15a37bbcefb709f52a9a482a5f71febe4b
Aditya Singh <155234975+adityasingh-0803@users.noreply.github.com>
2025-06-19 14:25:39 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import FrontendBattlePage from './FrontendBattlePage';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FrontendBattlePage />
+  </React.StrictMode>
+);
